bba6591583926210e8e294d4ac8531cd0e4e9a5a
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-07-13 09:09:08 -0300

    style: font import and images for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {},
   },
   plugins: [],
 };
