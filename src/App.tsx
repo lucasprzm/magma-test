b146e38b736392a526ea6a9954c414dd0fb59300
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-07-13 09:09:39 -0300

    feat: header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="text-bold flex justify-center">
-      <p>Hello World!</p>
+    <div>
+      <Header />
+      <div>
+        <p className="font-poppins">Teste Fonte Poppins</p>
+        <p className="font-inter">Teste Fonte Inter</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Logo from "../assets/Logo.svg";
+import burgerIcon from "../assets/icons/burger-icon.svg";
+import bellIcon from "../assets/icons/bell-icon.svg";
+import personIcon from "../assets/icons/person-icon.svg";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center w-full h-[60px] bg-[#4B2C65]">
+      <div className="flex items-center">
+        <div className="px-16">
+          <img src={Logo} alt="Logo PMO Soft" />
+        </div>
+        <div>
+          <img src={burgerIcon} alt="Menu" />
+        </div>
+      </div>
+      <div className="flex justify-around items-center">
+        <div>
+          <img src={bellIcon} alt="Notificações" />
+        </div>
+        <div className="px-4">
+          <img src={personIcon} alt="Perfil" />
+        </div>
+      </div>
+    </header>
+  );
+}
