37df334036625c79f3d45a0f962ed9a1dea321f6
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-07-14 17:22:21 -0300

    docs: color and screen configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
       poppins: ["Poppins", "sans-serif"],
       inter: ["Inter", "sans-serif"],
     },
-    colors: {},
+    colors: {
+      "orange-5": "#F2683A",
+      "purple-4": "#6D28D9",
+    },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1281px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
