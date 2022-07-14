b368c2e8a489bb0db9b931208fff1f2ebf0ad8ad
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-07-14 17:21:57 -0300

    style: component height fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ import personIcon from "../assets/icons/Header/person-icon.svg";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full h-[60px] bg-[#4B2C65]">
+    <header className="flex justify-between items-center h-full w-full bg-[#4B2C65]">
       <div className="flex items-center">
-        <div className="px-16">
+        <div className="px-12 desktop:px-14">
           <img src={Logo} alt="Logo PMO Soft" />
         </div>
         <div>
