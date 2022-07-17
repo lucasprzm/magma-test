14cda8043c1e74d0821058a3c22417317154b766
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-07-17 19:10:42 -0300

    style: new colors and new user image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,15 @@ module.exports = {
       inter: ["Inter", "sans-serif"],
     },
     colors: {
+      "orange-1": "#FAEDDE",
       "orange-4": "#FFA750",
       "orange-5": "#F2683A",
+      "purple-1": "#8B5CF6",
+      "purple-3": "#EDE9FE",
       "purple-4": "#6D28D9",
+      "gray-1": "#F5F5F5",
+      "gray-2": "#D4D4D4",
+      "gray-4": "#737373",
     },
     screens: {
       tablet: "640px",
